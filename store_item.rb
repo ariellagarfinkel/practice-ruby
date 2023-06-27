8694ae2911d87665cd403aa654abcb455c05b890
Ariella Garfinkel <ariellagarfinkel@gmail.com>
2023-06-27 14:33:00 -0500

    items with symbols

---
+++ store_item.rb
@@ -0,0 +1,11 @@
+coats = { :color => "Navy", :size => "M", :price => 80 }
+shirts = { :color => "Orange", :size => "S", :price => 20 }
+skirts = { :color => "Yellow", :size => "L", :price => 40 }
+
+p coats
+
+coats = { color: "Navy", size: "M", price: 80 }
+shirts = { color: "Orange", size: "S", price: 20 }
+skirts = { color: "Yellow", size: "L", price: 40 }
+
+p coats
