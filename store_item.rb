cfaa1e570a8022bef91ee6e1c52ddafb089f9833
Ariella Garfinkel <ariellagarfinkel@gmail.com>
2023-06-28 13:10:46 -0500

    input_options shortcuts

--- store_item.rb
+++ store_item.rb
@@ -1,57 +1,21 @@
-# coats = { :color => "Navy", :size => "M", :price => 80 }
-# shirts = { :color => "Orange", :size => "S", :price => 20 }
-# skirts = { :color => "Yellow", :size => "L", :price => 40 }
-
-# p coats
-
-# coats = { color: "Navy", size: "M", price: 80 }
-# shirts = { color: "Orange", size: "S", price: 20 }
-# skirts = { color: "Yellow", size: "L", price: 40 }
-
-# p coats
-
 class Shirts
   attr_reader :color, :size, :price
   attr_writer :color, :size, :price
 
-  def initialize(input_color, input_size, input_price)
-    @color = input_color
-    @size = input_size
-    @price = input_price
+  def initialize(input_options)
+    @color = input_options[:color]
+    @size = input_options[:size]
+    @price = input_options[:price]
   end
 
-  #reader method
-  # def input_color
-  #   @color
+  # def item_info
+  #   puts "This store sells shirts that are #{@color} and size #{@size} and #{@price} dollars."
   # end
+end
 
-  # #reader method
-  # def input_size
-  #   @size
-  # end
-
-  # #reader method
-  # def input_price
-  #   @price
-  # end
-
-  # #writer method
-  # def price=(input_price)
-  #   @price = input_price
-  # end
-
-  def item_info
-    puts "This store sells shirts that are #{@color} and size #{@size} and #{@price} dollars."
-  end
-
-  # def end_of_season_sale
+# def end_of_season_sale
 #    @price - 0.05%
 # end
 
-coats = Shirts.new("red", "M", "20")
-p coats
-p coats.item_info
-p coats.price
-coats.price = 25
-p coats.price
-
+shirts = Shirts.new ({ color: "Purple", size: "XL", price: "15" })
+p shirts
