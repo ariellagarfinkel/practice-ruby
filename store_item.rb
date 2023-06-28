3e1fbb7dd2c16b77283a9222c1df814d8e0f05a6
Ariella Garfinkel <ariellagarfinkel@gmail.com>
2023-06-28 12:31:56 -0500

    Turn store into a class

--- store_item.rb
+++ store_item.rb
@@ -1,11 +1,55 @@
-coats = { :color => "Navy", :size => "M", :price => 80 }
-shirts = { :color => "Orange", :size => "S", :price => 20 }
-skirts = { :color => "Yellow", :size => "L", :price => 40 }
+# coats = { :color => "Navy", :size => "M", :price => 80 }
+# shirts = { :color => "Orange", :size => "S", :price => 20 }
+# skirts = { :color => "Yellow", :size => "L", :price => 40 }
 
+# p coats
+
+# coats = { color: "Navy", size: "M", price: 80 }
+# shirts = { color: "Orange", size: "S", price: 20 }
+# skirts = { color: "Yellow", size: "L", price: 40 }
+
+# p coats
+
+class Shirts
+  def initialize(input_color, input_size, input_price)
+    @color = input_color
+    @size = input_size
+    @price = input_price
+  end
+
+  #reader method
+  def input_color
+    @color
+  end
+
+  #reader method
+  def input_size
+    @size
+  end
+
+  #reader method
+  def input_price
+    @price
+  end
+
+  #writer method
+  def price=(input_price)
+    @price = input_price
+  end
+
+  def item_info
+    puts "This store sells shirts that are #{input_color} and size #{input_size} and #{input_price} dollars."
+  end
+
+  def end_of_season_sale
+end
+
+coats = Shirts.new("red", "M", "20")
 p coats
+p coats.input_price
+coats.price = 25
+p coats.input_price
+p coats.item_info
 
-coats = { color: "Navy", size: "M", price: 80 }
-shirts = { color: "Orange", size: "S", price: 20 }
-skirts = { color: "Yellow", size: "L", price: 40 }
 
-p coats
+
