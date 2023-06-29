66a643ed8968763a93742274558f288a762d1fd3
Ariella Garfinkel <ariellagarfinkel@gmail.com>
2023-06-29 16:31:11 -0500

    added link/'require'

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
-"./store_item_classes"
-"./module_ClothingAble"
-shirts = Shirts.new ({ color: "Purple", size: "XL", price: "15", sleeves: "Three Quarter" })
+require "./store_item_classes"
+require "./module_ClothingAble"
+shirts = Shirts.new (color: "Purple", size: "XL", price: "15", sleeves: "Three Quarter")
 p shirts
-skirts = Skirts.new ({ color: "Purple", size: "XL", price: "15", length: "30 inches" })
+skirts = Skirts.new (color: "Purple", size: "XL", price: "15", length: "30 inches")
 p shirts
 shirts.color
 shirts.color = ("red")
---
+++ practice_problems.rb
@@ -0,0 +1,42 @@
+# puts "List your 5 favorite foods"
+# fav_foods = []
+# index = 0
+# 5.times do
+#   response = gets.chomp
+#   fav_foods << response
+# end
+# p fav_foods
+
+# index_1 = 0
+# index_2 = 1
+
+# 5.times do
+#   puts "#{index_2}. #{fav_foods[index_1]}"
+#   index_1 += 1
+#   index_2 += 1
+# end
+
+#1C.
+
+# count = 0
+# index = 0
+
+# while count < 11
+#   p count
+#   index += 1
+#   count += 1
+# end
+
+# 2
+puts "Name the recipes Sam can make"
+while 
+recipes = gets.chomp
+sams_recipes = []
+sams_recipes << recipes
+p sams_recipes
+
+puts "Name the languages Sally can speak"
+sallys_languages = []
+languages = gets.chomp
+sallys_languages << languages
+p sallys_languages
